83d87cdfc5bd05bef74bdc7a384d13334dc8ee31
Patrik Keppeler <patrikkeppeler@gmail.com>
2019-11-30 12:59:05 +0100

    merge

--- WS19_G6_LevenshteinDistance/UnitTests/LevenshteinDistanceTest.py
+++ WS19_G6_LevenshteinDistance/UnitTests/LevenshteinDistanceTest.py
@@ -1,24 +1,58 @@
 import unittest
+from  WS19_G6_LevenshteinDistance import LevenshteinDistance
 
 class Test_LevenshteinDistanceTest(unittest.TestCase):
-
-
+    def setUp(self):
+        self.levenshtein = LevenshteinDistance()
+        self.levenshtein.matrix=[[]]
+     
     def levensteinD_Test(self):
+        #Assume
+        matrix 
+        #Action
+
+        #Assert
         self.fail("Not implemented")
 
     def insert_Test(self):
+        #Assume
+
+
+        #Action
+
+        #Assert
         self.fail("Not implemented")
 
     def delete_Test(self):
+        #Assume
+
+        #Action
+
+        #Assert
         self.fail("Not implemented")
 
     def replace_Test(self):
+        #Assume
+
+        #Action
+
+        #Assert
         self.fail("Not implemented")
 
     def read_Test(self):
+        #Assume
+
+        #Action
+
+        #Assert
         self.fail("Not implemented")
 
     def write_Test(self):
+        #Assume
+
+        #Action
+
+        #Assert
         self.fail("Not implemented")
 
 if __name__ == '__main__':
