b2af5740d14603e355887b6989fb34a94bd34e10
Dennis Zeller <dennis.zhukki@gmail.com>
2019-11-30 11:49:11 +0100

    matrix class

---
+++ WS19_G6_LevenshteinDistance/Matrix.py
@@ -0,0 +1,9 @@
+
+class Matrix(object):
+    """description of class"""
+
+    def __init__(self, stringS, stringT):
+
+        self.matrix = [[0 for x in range(len(stringS))] for y in range(len(stringT))]
+
+
