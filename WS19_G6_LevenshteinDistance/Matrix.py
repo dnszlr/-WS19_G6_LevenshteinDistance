f9d3a4386ecd22e39339e72868a49694b4f64606
Dennis Zeller <dennis.zhukki@gmail.com>
2019-11-30 12:20:01 +0100

    matrix constructor

--- WS19_G6_LevenshteinDistance/Matrix.py
+++ WS19_G6_LevenshteinDistance/Matrix.py
@@ -4,6 +4,11 @@ class Matrix(object):
 
     def __init__(self, stringS, stringT):
 
-        self.matrix = [[0 for x in range(len(stringS))] for y in range(len(stringT))]
+        self.matrix = [[0 for x in range(len(stringS) + 1)] for y in range(len(stringT) + 1)]
+
+        for i in range(0, len(self.matrix)):
+            matrix[i][0] = i
+            for j in range(0, len(matrix[i])):
+                matrix[0][j] = j
 
 
