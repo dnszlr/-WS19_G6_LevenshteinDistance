8bce5637f55b56a9bccec0f5c5786362252a357e
kalash96 <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2019-11-30 16:28:25 +0100

    class CardRepair

--- WS19_G6_LevenshteinDistance/CardRepair.py
+++ WS19_G6_LevenshteinDistance/CardRepair.py
@@ -1,2 +1,29 @@
 class CardRepair(object):
     #description of class
+
+    def __init__(self, card, allCards):
+        self.card = card
+        self.allCards = allCards
+        self.matrix = [[]]
+
+    #create the LD-matrix
+    def LD(self):
+        return self.matrix
+
+    #repair the name of the card from the cards list
+    def repair(self, brokenCard, allCards):
+        return self.card
+
+    #inserts the letter c at the index i
+    def insert(self, i, c):
+        pass
+
+    #deletes the letter at the index i
+    def delete(self, i):
+        pass
+
+    #replaces the letter at index i with c.
+    def replace(self, i, c):
+        pass
+
+
