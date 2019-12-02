4495dad41c52bb49c4e7c00a7d9383330ae1bb3b
Philipp <lohmann.philipp@gmx.de>
2019-12-02 16:08:37 +0100

    UnitTest CardRepair

--- WS19_G6_LevenshteinDistance/UnitTests/CardRepairTest.py
+++ WS19_G6_LevenshteinDistance/UnitTests/CardRepairTest.py
@@ -10,9 +10,13 @@ class Test_CardRepairTest(unittest.TestCase):
         card3 = Card("halloogo", 2,4,5,6)
         card4 = Card("hallooffrfo", 2,4,5,6)
         list1 = [card2, card3, card4]
-        cardRepair1 = CardRepair(card1, list1)#
+        cardRepair1 = CardRepair(card1, list1)
         assert(cardRepair1.LD(list1[1]) is not None)
         assert(card1 is not None)
+        for i in range(len(cardRepair1.matrix)):
+            for j in range(len(cardRepair1.matrix[i])):
+                print(cardRepair1.matrix[i][j], end=' ')
+            print()
        # self.fail("Not implemented")
 
 if __name__ == '__main__':
