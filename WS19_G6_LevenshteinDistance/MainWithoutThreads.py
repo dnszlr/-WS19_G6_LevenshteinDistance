6205547317f5fe021f48ca10b51ca9b242695dfa
Krays <Krays@LAPTOP-T73PK2J2>
2019-12-09 16:48:12 +0100

    main without threads

--- WS19_G6_LevenshteinDistance/MainWithoutThreads.py
+++ WS19_G6_LevenshteinDistance/MainWithoutThreads.py
@@ -1,4 +1,20 @@
-class MainWithoutThreads(object):
-    """description of class"""
+from CardCollector import CardCollector
+from CardRepair import CardRepair
+
+
+if __name__=='__main__':
+        collector1 = CardCollector()
+
+        collector1.buildScrambled("scrambled")
+        collector1.buildReference("reference")
+      
+        collector1.getRepairedCardsList()
+
+        #print(collector1.repairedCards[0].name)
+        #print(len(collector1.repairedCards))
+        
+        collector1.writeFile("ArbeitspferdOhneThreads")
+
+
 
 
