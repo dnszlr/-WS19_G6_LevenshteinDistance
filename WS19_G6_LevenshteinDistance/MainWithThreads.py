24b39cfaecd739c730ea1562fc6c65a2f21b4374
kalash96 <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2019-12-10 16:24:30 +0100

    Threads for buildScrambled and buildReference

--- WS19_G6_LevenshteinDistance/MainWithThreads.py
+++ WS19_G6_LevenshteinDistance/MainWithThreads.py
@@ -1,7 +1,7 @@
 from CardCollector import CardCollector
 from CardRepair import CardRepair
 from multiprocessing import Pool
-from multiprocessing import Process
+from threading import Thread
 import time
 import sys
 
@@ -9,12 +9,14 @@ startTime = time.time()
 if __name__=='__main__':
         collector1 = CardCollector()
         
-        #process1 = Process(target = collector1.buildScrambled, args=("scrambled",))
-        #process1.start()
-        #process1.join()
-        #process1.close()
-        collector1.buildScrambled("scrambled")
-        collector1.buildReference("reference")
+        t1 = Thread(target = collector1.buildScrambled, args = ("scrambled",))
+        t1.start()
+        t1.join()        
+        t2 = Thread(target = collector1.buildReference, args = ("reference",))
+        t2.start()
+        t2.join()
+        #collector1.buildScrambled("scrambled")
+        #collector1.buildReference("reference")
         numbers = range(len(collector1.brokenCards))
         pool = Pool(10)
         r = pool.map_async(collector1.getRepairedCard, numbers) 
